c9ad0c279c8ad678ffe88b2fc7823d530caa8c8e
GilShalev <gilshalev2014@gmail.com>
2024-12-10 19:17:56 +0200

    adding cosomos DB connection string

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,41 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-// Connect to MongoDB (replace with your connection string)
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://<your-db-url>', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Connection string to your MongoDB database
+// const connectionString = 'mongodb://localhost:27017';
+
+// // Connect to MongoDB
+// mongoose
+//   .connect(connectionString, {
+//   })
+//   .then(() => {
+//     console.log('Connected to MongoDB successfully');
+//   })
+//   .catch((err) => {
+//     console.error('Error connecting to MongoDB:', err);
+//   });
+
+
+const cosmosConnectionStr = process.env.MONGODB_CONNECTION_STRING;
+
+if (cosmosConnectionStr != null) {
+  mongoose
+    .connect(cosmosConnectionStr, {
+    })
+    .then(() => {
+      console.log('Connected to MongoDB successfully');
+    })
+    .catch((err) => {
+      console.error('Error connecting to Cosmos DB:', err);
+    });
+}
+// const client = new CosmosClient(cosmosConnectionStr);
+// const database = client.database("hair-sallon-clients");
+// const container = database.container("hair-sallon-clients-container");
+
+// // Example query
+// const { resources } = await container.items.query("SELECT * FROM c").fetchAll();
+// console.log(resources);
 
 // Define a simple route
 app.get('/', (req: Request, res: Response) => {
@@ -23,7 +53,6 @@ app.get('/', (req: Request, res: Response) => {
 
 // Define routes (could be in a separate file)
 app.post('/clients', (req: Request, res: Response) => {
-  // Your database interaction logic here
   res.send('Client data saved!');
 });
 
@@ -31,3 +60,12 @@ app.post('/clients', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+//const cosmosConnectionString = process.env.COSMOS_DB_CONNECTION_STRING;
+// const USERNAME = 'hairsallonbacke-server';
+// const HOST = 'hairsallonbacke-server.mongo.cosmos.azure.com';
+// const PORT = 10255;
+// const PASSWORD = 'DioKgt52Sax1DP1So09ea0yBrDGnmvwOLBFFGKgaCQcM1EM4NuPvWEsSLodSZec8ayqYkfsufEC2ACDbirSBJw==';
+// const cosmosConnectionStr = `mongodb://${USERNAME}:${PASSWORD}@${HOST}:${PORT}/?ssl=true&replicaSet=globaldb&retrywrites=false`;
+//const cosmosConnectionStr = 'mongodb://hairsallonbacke-server:DioKgt52Sax1DP1So09ea0yBrDGnmvwOLBFFGKgaCQcM1EM4NuPvWEsSLodSZec8ayqYkfsufEC2ACDbirSBJw==@hairsallonbacke-server.mongo.cosmos.azure.com:10255/hairsallonbacke-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@hairsallonbacke-server@';//process.env.MONGODB_CONNECTION_STRING;
